5bbe74f52ad5e636ab30dbf489288137badc1608
RatSteak <ratsteak@protonmail.com>
2020-04-03 23:54:45 +0200

    added tcp_scanner.py

---
+++ python/tcp_scanner.py
@@ -0,0 +1,25 @@
+import sys
+import traceback
+import socket
+
+def knocking_port(ip,port):
+    tcp(ip,port)
+
+def tcp(ip,port):
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    con = p.connect_ex((ip,port))
+    if con == 0:
+        print("TCP "+str(port))
+    p.close
+
+try:
+    for cont in range(1,int(sys.argv[1])):
+        knocking_port(sys.argv[2],cont)
+except:
+    traceback.print_exc()
+    print("USAGE python tcp_scanner.py MAXPORT IP")
+
+
+
+
