cd169ee6bf7b183d0daaa6ce54fd20d724603aa4
RatSteak <ratsteak@protonmail.com>
2020-04-03 22:17:47 +0200

    new folder python and wget.py

---
+++ python/wget.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+
+try:
+	d = requests.get(str(sys.argv[1]))
+	f = open(str(sys.argv[1]).split("://")[1], 'wb')
+	f.write(d.content)
+except:
+	print(sys.exc_info()[0])
+	print("Usage: python wget_python fullurl")
+
+
+
